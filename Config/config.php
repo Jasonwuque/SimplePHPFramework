e21aa1e9e3ebaf0f6ea394cfacffa41c3c4bb940
Jasonwuque <2541553191@qq.com>
2016-11-17 21:35:38 +0800

    first

---
+++ Config/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'DB_HOST' => 'localhost',
+	'DB_USER' => 'root',
+	'DB_PWD' => 'liwenkaihaha',
+	'DB_NAME' => 'user',
+	'DB_CHARSET' => 'utf8',
+	'DB_PREFIX' => 'bbs_',
+	'CACHE_TIME' => '3600',
+	'CACHE_DIR' => './cache/',
+	'TPL_DIR' => './App/Views/',
+	
+];
